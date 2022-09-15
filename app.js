a59ea99a8bbc8fbf022412d0ca971ce4eecd1d29
Zer0cheros <christian.wiss@optimaedu.fi>
2022-09-15 15:28:52 +0300

    update

--- app.js
+++ app.js
@@ -10,13 +10,18 @@ window.onscroll = ()=>{
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         navbar.classList.add('navbar-scroll')
         navbarText.forEach(text => text.style.color = '#ffff')
-        content.style.top = '80%'
+        setTimeout(()=>{
+          studies.style.top = '110%'
+          console.log(document.documentElement.scrollTop);
+        },1500)
+        
        } else {
         navbar.classList.remove('navbar-scroll')
         navbarText.forEach(text => text.style.color = '#000')
-        content.style.top = '100%'  
+        studies.style.top = '120%'
       }
 }
+
 function color(){
   if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
     rightText.style.color = 'black'
@@ -29,12 +34,6 @@ function color(){
     leftText.style.color = 'var(--text-color)'
   }
 }
-function slideUp(){
-  if(document.body.scrollTop > 330 || document.documentElement.scrollTop > 330){
-    studies.style.top = '130%' 
-  }
-  else {
-    studies.style.top = '180%' 
-  }
-}
+
+
   
